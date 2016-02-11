e8183848d3a386eb9e83e5dfdc373bdbebf62ffc
DominicRossillo <domrossillo@Optonline.net>
2016-02-11 00:02:59 -0500

    Adding the multi-dimentional  array representing the dfa

--- src/ManWolf.java
+++ src/ManWolf.java
@@ -12,5 +12,84 @@ public class ManWolf {
 	private static final int q7 = 7;
 	private static final int q8 = 8;
 	private static final int q9 = 9;
+	private static final int q10 = 10;
+	
+	int [][] DFAArray = {
+			//q0
+			{q10,q10,q1,q10,q10},
+			//q1
+			{q10,q10,q0,q10,q2},
+			//q2
+			{q10,q8,q10,q3,q2},
+			//q3
+			{q10,q10,q4,q2,q10},
+			//q4
+			{q10,q5,q3,q10,q10},
+			//q5
+			{q10,q4,q10,q8,q6},
+			//q6
+			{q10,q10,q7,q10,q5},
+			//q7
+			{q10,q10,q6,q10,q10},
+			//q8
+			{q10,q2,q9,q10,q10},
+			//q9
+			{q10,q10,q8,q5,q10},
+			// error state q10
+			{q10,q10,q10,q10,q10}
+			
+			
+	};
+	
+	static private int DFAstate(int s, char c) {
+	    switch (s) {
+	      case q0: switch (c) {
+	            case '0': return q0;
+	            case '1': return q1; 
+	            default: return q3;
+	          }
+	      case q1: switch (c) {
+	            case '0': return q2;
+	            case '1': return q0;
+	            default: return q3;
+	          }
+	      case q2: switch (c) {
+	            case '0': return q1;
+	            case '1': return q2; 
+	            default: return q3;
+	          }
+	      
+	      case q4: switch (c){
+	      		case '0': return q1;
+	      }
+	      
+	      case q5: switch (c){
+    		    case '0': return q1;
+	      }
+	      
+	      case q6: switch (c){
+	      		case '0': return q1;
+	      }
+    
+	      
+	      
+	      case q7: switch (c){
+    			case '0': return q1;
+	      }
+    
+	      case q8: switch (c){
+	      		case '0': return q1;
+	      }
+    
+	      case q9: switch (c){
+    			case '0': return q1;
+	      }
+	      
+	      default: return q3;
+	    }
+	    
+	    
+	}
+
 	
 }
