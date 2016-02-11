b76a90dfd6d05fc6d859ef2301240241abebb037
DominicRossillo <domrossillo@Optonline.net>
2016-02-10 23:14:11 -0500

    first commit
    start of project

---
+++ src/ManWolf.java
@@ -0,0 +1,16 @@
+
+public class ManWolf {
+
+	
+	private static final int q0 = 0;
+	private static final int q1 = 1;
+	private static final int q2 = 2;
+	private static final int q3 = 3;
+	private static final int q4 = 4;
+	private static final int q5 = 5;
+	private static final int q6 = 6;
+	private static final int q7 = 7;
+	private static final int q8 = 8;
+	private static final int q9 = 9;
+	
+}
